35351bc81221d67687353653df1ed4d12d74334a
jonestremblay <jonathanrtremblay@gmail.com>
2020-10-31 15:35:03 -0400

    Cleaned code : fixed all IDE's warning for a fresh start.

--- banking/banking.py
+++ banking/banking.py
@@ -1,24 +1,34 @@
-import math
 import random
 import sqlite3
 
 # Connect to database and create a cursor instance.
 conn = sqlite3.connect("card.s3db")
-c = conn.cursor()
+cursor = conn.cursor()
 
-c.execute("""CREATE TABLE IF NOT EXISTS card (
+cursor.execute("""CREATE TABLE IF NOT EXISTS card (
         id INTEGER ,
         number TEXT,
         pin TEXT,
         balance INTEGER DEFAULT 0
          )""")
+conn.commit()
+
+
+def remove_symbols_from_string(stringToEdit) -> str:
+    """
+    Convert the fetch into a string, then removes symbols   [ ] ( ) ,    from the string.
+    """
+    string = str(stringToEdit)
+    for z in ("[", ""), ("]", ""), ("(", ""), (")", ""), (",", ""):
+        string = string.replace(*z)
+    return string
 
 
 def gen_acc_number() -> str:
-    acc_number = ""
-    while len(acc_number) < 9:
-        acc_number += str(random.randint(0, 9))
-    return acc_number
+    acc_num = ""
+    while len(acc_num) < 9:
+        acc_num += str(random.randint(0, 9))
+    return acc_num
 
 
 def gen_pin_code() -> str:
@@ -28,97 +38,176 @@ def gen_pin_code() -> str:
     return pin_code
 
 
+def gen_check_digit(bank_idNumber, account_number) -> str:
+    """
+    Luhn's algorithm: all numbers % 10 == 0
+    so sum of all numbers of the card number - without check digit - (sum)
+    (sum + x) % 10 == 0
+    Returns the check-digit that pass Luhn's algorithm
+    """
+    count_list = list(bank_idNumber) + list(account_number)
+    int_count_list = [int(k) for k in count_list]
+    new_list = []
+    check_digit = ""
+    for a in range(0, len(int_count_list), 2):
+        new_list.append(int_count_list[a] * 2)
+    for b in range(1, len(int_count_list), 2):
+        new_list.append(int_count_list[b])
+    c = 0
+    for k in new_list:
+        if k > 9:
+            c += 9
+    int_sum = sum(new_list) - c
+    # poss_check_digits = []
+    for x in range(10):
+        if (int_sum + x) % 10 == 0:
+            # poss_check_digits.append(x)
+            check_digit = str(x)
+    # check_digit = str(random.choice(poss_check_digits))
+    return check_digit
+
+
 class CardNumber:
 
-    def __init__(self, bin, acc_number, pin_code):
-        self.bin = bin
-        self.acc_number = acc_number
-        self.pin_code = pin_code
+    def __init__(self, _bin, _acc_number, _pin_code):
+        self.bin = _bin
+        self.acc_number = _acc_number
+        self.pin_code = _pin_code
         self.card_number_no_check_digit = self.bin + self.acc_number
         self.balance = 0
 
-    @staticmethod
-    def gen_check_digit() -> str:
-        """
-        Luhn's algorithm: all numbers % 10 == 0
-        so sum of all numbers of the card number - without check digit - (sum)
-        (sum + x) % 10 == 0
-        """
-        count_list = list(account.bin) + list(account.acc_number)
-        int_count_list = [int(i) for i in count_list]
-        new_list = []
-
-        for a in range(0, len(int_count_list), 2):
-            new_list.append(int_count_list[a] * 2)
-        for b in range(1, len(int_count_list), 2):
-            new_list.append(int_count_list[b])
-        c = 0
-        for i in new_list:
-            if i > 9:
-                c += 9
-        int_sum = sum(new_list) - c
-        poss_check_digits = []
-        for x in range(10):
-            if (int_sum + x) % 10 == 0:
-                # poss_check_digits.append(x)
-                check_digit = str(x)
-        # check_digit = str(random.choice(poss_check_digits))
-        return check_digit
-
 
 iin_bin = "400000"
-
 choice = -1
 while choice != 0:
     choice = input("1. Create an account\n"
                    "2. Log into account\n"
+                   "3. View all accounts\n"
                    "0. Exit\n")
     if choice == "1":
-        account = CardNumber(iin_bin, gen_acc_number(), gen_pin_code())
-        check_sum = CardNumber.gen_check_digit()
-        card_number = account.bin + account.acc_number + check_sum
-        card_pin = account.pin_code
+        acc_number = gen_acc_number()
+        check_sum = gen_check_digit(iin_bin, acc_number)
+        card_number = iin_bin + acc_number + check_sum
+        card_pin = gen_pin_code()
         print("\nYour card has been created\n"
               "Your card number:\n" +
               card_number + "\n"
-              "Your card PIN:\n" +
+                            "Your card PIN:\n" +
               card_pin + "\n")
-        card_balance = account.balance
-        card_id = account.bin
-        card = (card_id, card_number, card_pin, card_balance)
-        c.execute("INSERT INTO card VALUES (?, ?, ?, ?)", card)
+        card = (iin_bin, acc_number + check_sum, card_pin, 0)
+        cursor.execute("INSERT INTO card (id, number, pin, balance) VALUES (?, ?, ?, ?)", card)
         conn.commit()
-        print("New card added to the card\'s database.")
-        print(c.fetchall())
+        # print("New card added to the card\'s database.")
+        # print("BIN: " + card[0] + " Acc_#: " + card[1] + "\n"
+        #       "PIN: " + card[2] + "   Balance: " + str(card[3]) + " $" + "\n")
         continue
     elif choice == "2":
         # Log into acc
         log_in = False
         user = input("Enter you card number:\n")
         pin = input("Enter your PIN:\n")
-
-        if user == (account.card_number_no_check_digit + CardNumber.gen_check_digit()) \
-                and pin == account.pin_code:
-            print("\nYou have successfully logged in!")
-            log_in = True
-            while log_in:
-                choice = input("\n1. Balance\n"
-                               "2. Log out\n"
-                               "0. Exit\n")
-                if choice == "1":
-                    balance = account.balance
-                    print("\nBalance: ", balance)
-                    continue
-                elif choice == "2":
-                    print("\nYou have successfully logged out!\n")
-                    log_in = False
-                elif choice == "0":
-                    print("\nBye!")
-                    conn.close()
-                    exit()
+        user_number = user[6:]
+        cursor.execute("SELECT number FROM card WHERE number == ? ", (user_number,))
+        user_fetch = cursor.fetchall()
+        user_data = remove_symbols_from_string(user_fetch)
+        cursor.execute("SELECT pin FROM card WHERE number == ? ", (user_number,))
+        pin_fetch = cursor.fetchall()
+        pin_data = remove_symbols_from_string(pin_fetch)
+        card_number = user
+        # user[6:]: slice the BIN (400000) from the input, to compare only acc_number, not card number.
+        if user_number in user_data:
+            if pin in pin_data:
+                print("\nYou have successfully logged in!")
+                log_in = True
+                while log_in:
+                    menu = input("\n1. Balance\n"
+                                 "2. Add income\n"
+                                 "3. Do transfer\n"
+                                 "4. Close account\n"
+                                 "5. Log out\n"
+                                 "0. Exit\n")
+                    if menu == "1":
+                        # Print balance
+                        acc = (user_number,)
+                        cursor.execute("SELECT balance FROM card WHERE number == ? ", acc)
+                        fetch = cursor.fetchall()
+                        balance = remove_symbols_from_string(fetch)
+                        print("\nBalance: " + balance)  # + " $")
+                        continue
+                    elif menu == "2":
+                        # Add income
+                        income = int(input("Enter income:\n"))
+                        data = (income, user_number)
+                        cursor.execute("UPDATE card SET balance=(balance + ?) WHERE number == ? ", data)
+                        conn.commit()
+                        print("\nIncome was added!")
+                        continue
+                    elif menu == "3":
+                        # Transfer money to another card.
+                        cursor.execute("SELECT balance FROM card WHERE number == ?", (user_number,))
+                        fetch = cursor.fetchall()
+                        current_balance = int(remove_symbols_from_string(fetch))
+                        print("\nTransfer")
+                        card_num_to_transfer = input("Enter card number:\n")
+                        check_sum = gen_check_digit(iin_bin, card_num_to_transfer[6:])
+                        if card_num_to_transfer[6:] == user_number:
+                            print("\nYou can't transfer money to the same account!\n")
+                            break
+                        if check_sum == card_num_to_transfer[-1]:
+                            # Means it pass Luhn's algorithm.
+                            pass
+                        else:
+                            print("\nProbably you made mistake in the card number. Please try again!\n")
+                            break
+                        cursor.execute("SELECT number FROM card WHERE number == ?", (card_num_to_transfer[6:],))
+                        nums = cursor.fetchall()
+                        if (card_num_to_transfer[6:],) in nums:
+                            amount = int(input("Enter how much money you want to transfer:\n"))
+                            if amount <= current_balance:
+                                # do transfer and print success.
+                                sender = (amount, user_number)
+                                receiver = (amount, card_num_to_transfer[6:])
+                                cursor.execute("UPDATE card SET balance=(balance - ?) WHERE number == ? ", sender)
+                                cursor.execute("UPDATE card SET balance=(balance + ?) WHERE number == ? ", receiver)
+                                conn.commit()
+                                print("Success!")
+                            else:
+                                print("Not enough money!")
+                                continue
+                        else:
+                            print("Such a card does not exist.")
+                            continue
+                        pass
+                    elif menu == "4":
+                        # Close account
+                        cursor.execute("DELETE FROM card WHERE number == ?", (user_number,))
+                        conn.commit()
+                        print("\nThe account has been closed!\n")
+                        break
+                    elif menu == "5":
+                        # Log out
+                        print("\nYou have successfully logged out!\n")
+                        log_in = False
+                    elif menu == "0":
+                        print("\nBye!")
+                        conn.close()
+                        exit()
+                    else:
+                        print("Incorrect parameter.")
+                        continue
+            else:
+                print("\nWrong card number or PIN!\n")
+                continue
         else:
             print("\nWrong card number or PIN!\n")
             continue
+    elif choice == "3":
+        # View all accounts
+        cursor.execute("SELECT * FROM card")
+        data = cursor.fetchall()
+        for i in data:
+            print(i)
+        print()
     elif choice == "0":
         # Exit
         print("Bye!")
@@ -126,3 +215,5 @@ while choice != 0:
         exit()
     else:
         print("Incorrect parameter.")
+        conn.close()
+        exit()
